e61453d5a66fe6970e287333142beedb16f12846
darlingson <dmakuwila@gmail.com>
2023-11-20 21:51:48 +0200

    added color to the app bar

--- app/src/main/java/com/codeshinobi/bajeti/activities/MainOption.kt
+++ app/src/main/java/com/codeshinobi/bajeti/activities/MainOption.kt
@@ -26,6 +26,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
@@ -53,12 +54,17 @@ class MainOption : ComponentActivity() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainView(){
+
 //    val scaffoldState = rememberScaffoldState(rememberDrawerState(DrawerValue.Closed))
     Scaffold(
         topBar = {
             TopAppBar(
+                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
                 title = {
-                    Text("Top app bar")
+                    Text("Food Items", textAlign = TextAlign.Center, modifier = Modifier.fillMaxWidth())
                 }
             )
         },
