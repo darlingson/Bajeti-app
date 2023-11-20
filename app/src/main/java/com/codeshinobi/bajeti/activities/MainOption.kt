dc65a9a439dc3f45dc5f9e6328e92688452f8a33
darlingson <dmakuwila@gmail.com>
2023-11-20 21:35:39 +0200

    added a scafford, app bar and a floating button

--- app/src/main/java/com/codeshinobi/bajeti/activities/MainOption.kt
+++ app/src/main/java/com/codeshinobi/bajeti/activities/MainOption.kt
@@ -10,14 +10,22 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
@@ -42,11 +50,34 @@ class MainOption : ComponentActivity() {
         }
     }
 }
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainView(){
-    Column() {
+//    val scaffoldState = rememberScaffoldState(rememberDrawerState(DrawerValue.Closed))
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text("Top app bar")
+                }
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = { }) {
+                Icon(Icons.Default.Add, contentDescription = "Add")
+            }
+        }
+
+    )
+    {innerPadding ->
+    Column(
+        modifier = Modifier
+            .padding(innerPadding)
+    ) {
         Greeting2("Android")
         AmountsSection()
+        FAB()
+    }
     }
 }
 @Composable
@@ -59,6 +90,14 @@ fun Greeting2(name: String, modifier: Modifier = Modifier.height(250.dp)) {
             modifier = modifier
         )
         Text(text = "Total Amount reserved for Food : K 80,000")
+    }
+}
+@Composable
+fun FAB() {
+    FloatingActionButton(
+        onClick = {  },
+    ) {
+        Icon(Icons.Filled.Add, "Floating action button.")
     }
 }
 @Composable
@@ -93,15 +132,18 @@ fun AmountsCard(foodItem:String, modifier: Modifier, amount:String){
             .fillMaxWidth(),
     ) {
         Row(
+            Modifier.fillMaxWidth()
         ) {
             Text(text = foodItem,
                 modifier = Modifier
-                    .padding(16.dp),
-                textAlign = TextAlign.Center,)
+                    .padding(16.dp)
+                    .width(200.dp),
+                textAlign = TextAlign.Left,)
             Text(text = amount,
                 modifier = Modifier
-                    .padding(16.dp),
-                textAlign = TextAlign.End,)
+                    .padding(16.dp)
+                    .width(200.dp),
+                textAlign = TextAlign.Right,)
         }
     }
 }
