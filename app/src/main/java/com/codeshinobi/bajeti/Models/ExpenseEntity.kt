cb8d006b0154888de84f8908a428fec9a9305b72
darlingson <dmakuwila@gmail.com>
2023-11-21 20:40:54 +0200

    added Database object

--- app/src/main/java/com/codeshinobi/bajeti/Models/ExpenseEntity.kt
+++ app/src/main/java/com/codeshinobi/bajeti/Models/ExpenseEntity.kt
@@ -1,10 +1,14 @@
 package com.codeshinobi.bajeti.Models
+
+import android.content.Context
 import androidx.room.ColumnInfo
 import androidx.room.Database
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import androidx.room.Room.databaseBuilder
 import androidx.room.RoomDatabase
 import com.codeshinobi.bajeti.DAOs.ExpenseDAO
+
 
 @Entity
 data class ExpenseEntity(
@@ -14,7 +18,31 @@ data class ExpenseEntity(
     @ColumnInfo(name = "category")val Category: String?,
     @ColumnInfo(name = "amount")val amount: Int?
 )
+//@Database(entities = [ExpenseEntity::class], version = 1)
+//abstract class AppDatabase : RoomDatabase() {
+//    abstract fun ExpenseDAO(): ExpenseDAO
+//}
+//val db = Room.databaseBuilder(
+//    applicationContext,
+//    AppDatabase::class.java, "expense.db"
+//).build()
+
 @Database(entities = [ExpenseEntity::class], version = 1)
-abstract class AppDatabase : RoomDatabase() {
-    abstract fun userDao(): ExpenseDAO
+abstract class ExpensesDatabase : RoomDatabase() {
+    abstract val ExpenseDAO: ExpenseDAO?
+
+    companion object {
+        const val DATABASE_NAME = "expenses_db"
+        private var instance: ExpensesDatabase? = null
+        fun getInstance(context: Context): ExpensesDatabase? {
+            if (instance == null) {
+                instance = databaseBuilder(
+                    context.applicationContext,
+                    ExpensesDatabase::class.java,
+                    DATABASE_NAME
+                ).build()
+            }
+            return instance
+        }
+    }
 }
