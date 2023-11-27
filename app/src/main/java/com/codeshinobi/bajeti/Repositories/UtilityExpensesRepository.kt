fe72c7ca0adc8c74105d761c49d768aaef1192ea
darlingson <dmakuwila@gmail.com>
2023-11-27 20:17:31 +0200

    completed the utilities expense repository

--- app/src/main/java/com/codeshinobi/bajeti/Repositories/UtilityExpensesRepository.kt
+++ app/src/main/java/com/codeshinobi/bajeti/Repositories/UtilityExpensesRepository.kt
@@ -7,11 +7,36 @@ import com.codeshinobi.bajeti.DAOs.UtilitiesDAO
 import com.codeshinobi.bajeti.Models.TransportExpensesEntity
 import com.codeshinobi.bajeti.Models.UtilitiesEntity
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
 
 class UtilityExpensesRepository (private val UtilitiesDAO: UtilitiesDAO){
     val allTransportExpenses: LiveData<List<UtilitiesEntity>> = UtilitiesDAO.getAllUtilitiesExpenses()
     val searchResults = MutableLiveData<List<UtilitiesEntity>>()
     var sumofTransportExpenses: LiveData<Int> = UtilitiesDAO.getTotalUtilitiesAmount()
     private val coroutineScope = CoroutineScope(Dispatchers.Main)
+
+    fun insertUtilitiesExpense(newExpense: UtilitiesEntity) {
+        coroutineScope.launch(Dispatchers.IO) {
+            UtilitiesDAO.insertUtilities(newExpense)
+        }
+    }
+
+    fun deleteUtilitiesExpense(name: String) {
+        coroutineScope.launch(Dispatchers.IO) {
+            UtilitiesDAO.deleteUtilitiesExpense(name)
+        }
+    }
+
+    fun findUtilitiesExpense(name: String) {
+        coroutineScope.launch(Dispatchers.Main) {
+            searchResults.value = asyncFind(name).await()
+        }
+    }
+    private fun asyncFind(name: String): Deferred<List<UtilitiesEntity>?> =
+        coroutineScope.async(Dispatchers.IO) {
+            return@async UtilitiesDAO.findUtilitiesExpense(name)
+        }
 }
