a3605348a421c957e5da2cbd565afca60138a17e
darlingson <dmakuwila@gmail.com>
2023-12-03 17:51:05 +0200

    created month budget DAO

---
+++ app/src/main/java/com/codeshinobi/bajeti/DAOs/MonthBudgetDAO.kt
@@ -0,0 +1,16 @@
+package com.codeshinobi.bajeti.DAOs
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.codeshinobi.bajeti.Models.MonthBudget
+
+@Dao
+interface MonthBudgetDAO {
+    @Insert
+    fun insertTotalBudget(expense: MonthBudget)
+    @Query("SELECT * FROM MonthBudget")
+    fun getTotalBudget(): List<MonthBudget>
+    @Query("SELECT * FROM MonthBudget WHERE monthNumber = :month")
+    fun getMonthBudget(month: Int): List<MonthBudget>
+}
