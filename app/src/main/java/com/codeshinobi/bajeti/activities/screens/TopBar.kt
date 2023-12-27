294273385fae235cfed24c9d055b7aaa439a8ca0
darlingson <dmakuwila@gmail.com>
2023-12-27 11:41:28 +0200

    created the topbar

---
+++ app/src/main/java/com/codeshinobi/bajeti/activities/screens/TopBar.kt
@@ -0,0 +1,28 @@
+package com.codeshinobi.bajeti.activities.screens
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.vector.ImageVector
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopBar(title: String = "", buttonIcon: ImageVector, onButtonClicked: () -> Unit) {
+    TopAppBar(
+        title = {
+            Text(
+                text = title
+            )
+        },
+        navigationIcon = {
+            IconButton(onClick = { onButtonClicked() } ) {
+                Icon(buttonIcon, contentDescription = "")
+            }
+        }
+    )
+}
