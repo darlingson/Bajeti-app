f015e54cbd68504d77f626847f521ef01a1ee105
darlingson <dmakuwila@gmail.com>
2023-12-30 18:58:04 +0200

    added code from tutorial to use the camera

--- app/src/main/java/com/codeshinobi/bajeti/activities/ScanActivity.kt
+++ app/src/main/java/com/codeshinobi/bajeti/activities/ScanActivity.kt
@@ -2,6 +2,7 @@ package com.codeshinobi.bajeti.activities
 
 import android.net.Uri
 import android.os.Bundle
+import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -27,13 +28,20 @@ import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.viewinterop.AndroidView
 import coil.compose.rememberAsyncImagePainter
+import com.codeshinobi.bajeti.NoPermissionScreen
+import com.codeshinobi.bajeti.activities.screens.CameraScreen
 import com.codeshinobi.bajeti.activities.ui.theme.BajetiTheme
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.PermissionState
+import com.google.accompanist.permissions.isGranted
+import com.google.accompanist.permissions.rememberPermissionState
 
 class ScanActivity : ComponentActivity() {
     private lateinit var getContent: ActivityResultLauncher<String>
@@ -58,52 +66,63 @@ class ScanActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    ScanMainContent(getContent, imageUrlState.value)
+//                    ScanMainContent(getContent, imageUrlState.value)
+                    MainScreen()
                 }
             }
         }
     }
 }
-@OptIn(ExperimentalComposeUiApi::class)
-@Composable
-fun ScanMainContent(getContent: ActivityResultLauncher<String>, selectedImageUri: Uri?) {
-    var scannedText by remember {
-        mutableStateOf("")
-    }
-    val imageUrl = remember {
-        mutableStateOf<Uri?>(null)
-    }
-    Column(
-        Modifier
-            .fillMaxWidth()
-            .fillMaxHeight(),
-    ) {
-        Column(
-            Modifier
-                .fillMaxWidth()
-                .fillMaxHeight(0.8f),
-        ) {
-            ImageView(selectedImageUri)
-        }
-        Column(
-            Modifier
-                .fillMaxWidth()
-                .fillMaxHeight(),
-        ) {
-            Button(onClick = {
-                getContent.launch("image/*")
-            }) {
-                Text(text = "Select Image")
-            }
-            Button(onClick = { /*TODO*/ }) {
-                Text(text = "Scan Image")
-            }
-            AndroidView(factory = {
-                PreviewView(context = it)
-            })
-        }
-    }
-}
+//@OptIn(ExperimentalComposeUiApi::class)
+//@Composable
+//fun ScanMainContent(getContent: ActivityResultLauncher<String>, selectedImageUri: Uri?) {
+//    var scannedText by remember {
+//        mutableStateOf("")
+//    }
+//    val imageUrl = remember {
+//        mutableStateOf<Uri?>(null)
+//    }
+//    Column(
+//        Modifier
+//            .fillMaxWidth()
+//            .fillMaxHeight(),
+//    ) {
+//        Column(
+//            Modifier
+//                .fillMaxWidth()
+//                .fillMaxHeight(0.8f),
+//        ) {
+//            ImageView(selectedImageUri)
+//        }
+//        Column(
+//            Modifier
+//                .fillMaxWidth()
+//                .fillMaxHeight(),
+//        ) {
+//            Button(onClick = {
+//                getContent.launch("image/*")
+//            }) {
+//                Text(text = "Select Image")
+//            }
+//            Button(onClick = { /*TODO*/ }) {
+//                Text(text = "Scan Image")
+//            }
+//            AndroidView(factory = {
+//                PreviewView(context).apply {
+//                    layoutParams= LinearLayout.LayoutParams(
+//                        LinearLayout.LayoutParams.MATCH_PARENT,
+//                        LinearLayout.LayoutParams.MATCH_PARENT
+//                    )
+//                    implementationMode = PreviewView.ImplementationMode.COMPATIBLE
+//                    scaleType = PreviewView.ScaleType.FILL_CENTER
+//                }.also {
+//                    startTextRecognition(
+//                    )
+//                }
+//            })
+//        }
+//    }
+//}
 @Composable
 fun ImageView(selectedImageUri: Uri?) {
     Toast.makeText(LocalContext.current, selectedImageUri.toString(), Toast.LENGTH_SHORT).show()
@@ -119,4 +138,37 @@ fun ScanPreview() {
 //        Greeting("Darlingson")
 //        ScanMainContent(,)
     }
+}
+@OptIn(ExperimentalPermissionsApi::class)
+@Composable
+fun MainScreen() {
+
+    val cameraPermissionState: PermissionState = rememberPermissionState(android.Manifest.permission.CAMERA)
+
+    MainContent(
+        hasPermission = cameraPermissionState.status.isGranted,
+        onRequestPermission = cameraPermissionState::launchPermissionRequest
+    )
+}
+
+@Composable
+private fun MainContent(
+    hasPermission: Boolean,
+    onRequestPermission: () -> Unit
+) {
+
+    if (hasPermission) {
+        CameraScreen()
+    } else {
+        NoPermissionScreen(onRequestPermission)
+    }
+}
+
+@Preview
+@Composable
+private fun Preview_MainContent() {
+    MainContent(
+        hasPermission = true,
+        onRequestPermission = {}
+    )
 }
