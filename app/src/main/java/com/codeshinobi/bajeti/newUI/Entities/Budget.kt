f62fb33d8eda1f807d14f0feeaf84d5837e3de28
darlingson <dmakuwila@gmail.com>
2024-03-09 20:49:56 +0200

    converted the budget entity to a room database table data class

--- app/src/main/java/com/codeshinobi/bajeti/newUI/Entities/Budget.kt
+++ app/src/main/java/com/codeshinobi/bajeti/newUI/Entities/Budget.kt
@@ -1,9 +1,16 @@
 package com.codeshinobi.bajeti.newUI.Entities
 
+import androidx.annotation.NonNull
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+@Entity
 data class Budget(
-    val id: Int,
-    val amount: Double,
-    val monthName: String,
-    val monthNumber: Int,
-    val year: Int
+    @PrimaryKey(autoGenerate = true)
+    @NonNull
+    val id: Int = 0,
+    @ColumnInfo(name = "amount")val amount: Double,
+    @ColumnInfo(name = "month_name")val monthName: String,
+    @ColumnInfo(name = "month_number")val monthNumber: Int,
+    @ColumnInfo(name = "year")val year: Int
 )
