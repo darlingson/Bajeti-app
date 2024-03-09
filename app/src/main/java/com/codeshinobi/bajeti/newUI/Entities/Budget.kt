b2bd82cab37119a45a5faa03aad3c48fdd992d22
darlingson <dmakuwila@gmail.com>
2024-03-09 20:47:14 +0200

    added the budget entity

---
+++ app/src/main/java/com/codeshinobi/bajeti/newUI/Entities/Budget.kt
@@ -0,0 +1,9 @@
+package com.codeshinobi.bajeti.newUI.Entities
+
+data class Budget(
+    val id: Int,
+    val amount: Double,
+    val monthName: String,
+    val monthNumber: Int,
+    val year: Int
+)
