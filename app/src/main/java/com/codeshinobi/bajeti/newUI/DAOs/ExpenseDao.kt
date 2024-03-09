58019bf7a5f1b76c43c666f5bc8a0bee82d6353d
darlingson <dmakuwila@gmail.com>
2024-03-09 21:12:57 +0200

    added ExpenseDao

---
+++ app/src/main/java/com/codeshinobi/bajeti/newUI/DAOs/ExpenseDao.kt
@@ -0,0 +1,24 @@
+package com.codeshinobi.bajeti.newUI.DAOs
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.codeshinobi.bajeti.newUI.Entities.Expense
+
+@Dao
+interface ExpenseDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertExpense(expense: Expense)
+    @Update
+    suspend fun updateExpense(expense: Expense)
+    @Delete
+    suspend fun deleteExpense(expense: Expense)
+    @Query("SELECT * FROM expense WHERE id = :expenseId")
+    fun getExpenseById(expenseId: Int): LiveData<Expense>
+    @Query("SELECT * FROM expense WHERE month = :month AND year = :year")
+    fun getExpensesForMonth(month: String, year: Int): LiveData<List<Expense>>
+}
