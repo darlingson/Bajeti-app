7e7f81cf10c19f0ccca6f867bffcc6183e810e16
darlingson <dmakuwila@gmail.com>
2024-03-16 22:02:12 +0200

    added code to display list of expenses in the Expenses summary

--- app/src/main/java/com/codeshinobi/bajeti/newUI/HomeSummary.kt
+++ app/src/main/java/com/codeshinobi/bajeti/newUI/HomeSummary.kt
@@ -1,31 +1,38 @@
 package com.codeshinobi.bajeti.newUI
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.lifecycle.viewmodel.compose.viewModel
 import com.codeshinobi.bajeti.newUI.ViewModels.BudgetViewModel
-import com.codeshinobi.bajeti.newUI.ViewModels.ExpenseViewModel
 
 @Composable
 fun HomeSummary(viewModel: BudgetViewModel) {
@@ -33,13 +40,13 @@ fun HomeSummary(viewModel: BudgetViewModel) {
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .verticalScroll(rememberScrollState())
+//            .verticalScroll(rememberScrollState())
     ) {
         SummaryBudgetCard(viewModel)
         Divider(
             modifier = Modifier.padding(top = 8.dp, bottom = 8.dp)
         )
-        MonthlyExpensesSummaryList()
+        MonthlyExpensesSummaryList(viewModel)
     }
 }
 
@@ -89,16 +96,38 @@ fun SummaryBudgetCard(budgetVM: BudgetViewModel) {
 }
 
 @Composable
-fun MonthlyExpensesSummaryList() {
-    Text(text = "Expenses Summary")
-    Column {
-        ExpenseCard(name = "Spaghetti", amount = "1,200", category = "Food", date = "2023-05-01")
-        ExpenseCard(name = "Spaghetti", amount = "1,200", category = "Food", date = "2023-05-01")
-        ExpenseCard(name = "Spaghetti", amount = "1,200", category = "Food", date = "2023-05-01")
-        ExpenseCard(name = "Spaghetti", amount = "1,200", category = "Food", date = "2023-05-01")
-        ExpenseCard(name = "Spaghetti", amount = "1,200", category = "Food", date = "2023-05-01")
-        ExpenseCard(name = "Spaghetti", amount = "1,200", category = "Food", date = "2023-05-01")
-        ExpenseCard(name = "Spaghetti", amount = "1,200", category = "Food", date = "2023-05-01")
+fun MonthlyExpensesSummaryList(viewModel: BudgetViewModel) {
+    val expenses by viewModel.allExpenses.observeAsState(emptyList())
+    var searchText by remember { mutableStateOf("") }
+//    Column {
+//        ExpenseCard(name = "Spaghetti", amount = "1,200", category = "Food", date = "2023-05-01")
+//        ExpenseCard(name = "Spaghetti", amount = "1,200", category = "Food", date = "2023-05-01")
+//        ExpenseCard(name = "Spaghetti", amount = "1,200", category = "Food", date = "2023-05-01")
+//        ExpenseCard(name = "Spaghetti", amount = "1,200", category = "Food", date = "2023-05-01")
+//        ExpenseCard(name = "Spaghetti", amount = "1,200", category = "Food", date = "2023-05-01")
+//        ExpenseCard(name = "Spaghetti", amount = "1,200", category = "Food", date = "2023-05-01")
+//        ExpenseCard(name = "Spaghetti", amount = "1,200", category = "Food", date = "2023-05-01")
+//    }
+
+        Text(text = "Expenses Summary")
+        OutlinedTextField(
+            value = searchText,
+            onValueChange = { searchText = it },
+            label = { Text("Search Expenses") },
+            leadingIcon = { Icon(imageVector = Icons.Default.Search, contentDescription = null) },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 16.dp)
+        )
+    LazyColumn {
+        items(expenses.filter { it.name.contains(searchText, ignoreCase = true) }) { expense ->
+            ExpenseCard(
+                name = expense.name,
+                amount = expense.amount.toString(),
+                category = expense.category,
+                date = expense.date
+            )
+        }
     }
 }
 
